22853b2e017941c7f560c25da66b56918bc84870
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-10-18 16:55:24 -0300

    feature: adicionados endpoints de atualizacao das tabelas dos campeonatos

--- app/main.py
+++ app/main.py
@@ -1,14 +1,70 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from apscheduler.schedulers.background import BackgroundScheduler
+import pytz
+import json
+from services import update_league_data
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    """
+    Rota principal da API.
+
+    Retorna uma mensagem de saudação.
+    """
     return {"message": "Hello World"}
 
 
-@app.get("/confrontos")
-async def root():
-    league_ids = [39, 10]
-    return {"ligas": "premier, bundesliga, laliga, seriea, ligue1"}
+@app.on_event("startup")
+def start_recurring_update():
+    """
+    Inicia uma tarefa de atualização recorrente das ligas.
+
+    Esta função é executada na inicialização do aplicativo e agenda uma tarefa
+    para atualizar os dados das ligas diariamente às 6:30 da manhã.
+    """
+    tz = pytz.timezone('America/Sao_Paulo')
+    scheduler = BackgroundScheduler(timezone=tz)
+    scheduler.add_job(bulk_update_data, 'cron', hour=6, minute=30)
+    scheduler.start()
+
+
+@app.post("/bulk-update-standings-data/")
+async def bulk_update_data():
+    """
+    Atualiza os dados das ligas com base em um arquivo JSON.
+
+    Este endpoint lê um arquivo JSON contendo informações das ligas e realiza a
+    atualização dos dados de todas as ligas especificadas no arquivo.
+
+    Retorna uma mensagem de sucesso após a atualização.
+    """
+    try:
+        with open("leagues.json", "r") as file:
+            leagues = json.load(file)
+
+        for league in leagues:
+            await update_league_data(league["league_id"], league["season"])
+
+        return {"message": "Data updated successfully for all leagues"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/manual-update-standings-data/")
+async def update_data(league_id: int, season: int):
+    """
+    Atualiza manualmente os dados de uma liga específica.
+
+    Este endpoint permite a atualização manual dos dados de uma liga específica
+    com base no ID da liga e na temporada fornecidos como parâmetros.
+
+    Retorna uma mensagem de sucesso após a atualização.
+    """
+    try:
+        await update_league_data(league_id, season)
+        return {"message": "Data updated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
